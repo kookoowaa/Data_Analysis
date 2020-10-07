eeb7a474c925ae9b06bd1403d0f6739d99b44fd9
Park, Pablo Chanwoo <kookoowaa@gmail.com>
2020-10-07 22:23:34 +0900

    spider

---
+++ Crawling/Scrapy/myproject/myproject/spiders/news.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class NewsSpider(scrapy.Spider):
+    name = 'news'
+    allowed_domains = ['engadget.com']
+    start_urls = ['http://engadget.com/']
+
+    def parse(self, response):
+        pass
