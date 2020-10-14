eb2cafbae7295ec6b3c1115ca09f9758009d256b
Park, Pablo Chanwoo <kookoowaa@gmail.com>
2020-10-14 22:12:33 +0900

    updates

--- Crawling/Scrapy/myproject/myproject/spiders/news.py
+++ Crawling/Scrapy/myproject/myproject/spiders/news.py
@@ -6,14 +6,15 @@ class NewsSpider(scrapy.Spider):
     start_urls = ['http://engadget.com/']
 
     def parse(self, response):
-        link = response.css('a.o-hit_link::attr("href")').extract() # <a> 태그 중 <href> 태그를 필터
+        #link = response.css('a.o-hit_link::attr("href")').extract() # <a> 태그 중 <href> 태그를 필터
+        link = response.css('a::attr(href)').getall()
         for url in link:
-        	if url.find("products")==1:
+            if url.find("products")==1:
                 continue
             elif url == "#":
                 continue
             yield scrapy.Request(response.urljoin(url), self.parse_topics)
         link = filter(lambda x: x != '#', link) # 추출한 태그 중 속성이 '#'로 지정되는 경우 이를 제거
 	
-    def parse_topic(self, response):
+    def parse_topics(self, response):
         pass
