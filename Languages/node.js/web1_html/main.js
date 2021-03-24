060c4fc6638fe34ddaaa5de62cfb009f03fe0c07
Park, Pablo Chanwoo <kookoowaa@gmail.com>
2021-03-24 16:34:19 +0900

    dynamic html

--- Languages/node.js/web1_html/main.js
+++ Languages/node.js/web1_html/main.js
@@ -1,16 +1,42 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+ 
 var app = http.createServer(function(request,response){
-    var url = request.url;
+    let _url = `${request.headers.host}${request.url}`;
+    let objUrl = new URL(_url);
+    let _id = objUrl.searchParams.get('id');
+
     if(request.url == '/'){
-      url = '/index.html';
+      _id = 'Web';
     }
     if(request.url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    console.log(__dirname + url)
-    response.end(fs.readFileSync(__dirname + url));
+    fs.readFile(`d:/Git/Data_Analysis/Languages/node.js/web1_html/data/${_id}`, 'utf8', function(err,data){
+
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${_id}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${_id}</h2>
+        ${data}
+      </body>
+      </html>
+      `;
+      response.end(template);
+    })
  
 });
 app.listen(3000);
