84f6c31cd0b7ad96e1802dd4385347576b67af4f
Pablo Park <kookoowaa@gmail.com>
2021-03-30 14:44:12 +0900

    main with library

--- Languages/node.js/web1_html/main.js
+++ Languages/node.js/web1_html/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var qs = require('querystring');
+var template = require('./lib/template.js')
  
 var app = http.createServer(  function(request,response){
   let _url = `${__dirname}${request.url}`;
@@ -32,44 +33,10 @@ var app = http.createServer(  function(request,response){
     _id = 'Web';
   }
   
-  // html 구성요소들에 대한 함수 정의
-  var template = {
-    html: function(title, list, body){
-    return `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-        ${list}
-      </ul>
-      <a href='/create'>Create</a>
-      ${body}
-    </body>
-    </html>
-    `;
-    },
-
-    list: function(fileList){
-      let _tester = /[A-z]+\./   // 확장자 있는 파일 제외, data 폴더의 파일 리스트 반환
-      let _subtitle = ''
-      for(i=0;i<fileList.length;i++){
-        if((!_tester.test(fileList[i])) && (fileList[i]!='Web')){
-          _subtitle = _subtitle+ `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>` + '\n'
-        }
-      }
-      return _subtitle
-    }
-}
-
-
   
   var _tester = /web1_html\/$/;
-  if (_tester.test(_path)){
+  if (_tester.test(_path) || _path === '\\Users\\chpar10\\3D Objects\\Data_Analysis\\Languages\\node.js\\web1_html/data/coding.jpg'){
+    console.log(_path)
     // 동적 subtitle 할당을 위해 중복으로 fs 사용
     // global 변수 변경을 어떻게 할지 고민해 보아야 할 듯
     fs.readdir(`${_path}/data`, function(err,_flist){
@@ -179,7 +146,7 @@ var app = http.createServer(  function(request,response){
       })
     });
 
-    
+
   } else {
     response.writeHead(404);
     response.end('Not found')
